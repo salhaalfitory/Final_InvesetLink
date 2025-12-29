5edb2b2382b697106daa9427c4ee1ad942dface4
رهام الوزري <rehamalwezri@gmail.com>
2025-12-29 18:44:32 +0200

    ProjectController+Projectصفحات+DomainProfile+Project+ProjectVM+ProjectRepo

---
+++ InvestLink-DAL/Migrations/20251229010223_Initialmig.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestLink_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nationality",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nationality", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LegalBodyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Area = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeOfActivity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProposedSite = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AreaWanted = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeOfBenefitFromSite = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    projectTimeLine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConstructionPeriod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocalLoans = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ForeignLoans = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CostLandBuild = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CostMachine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CostSetup = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalCost = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Technology = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RawMaterialLocal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RawMaterialForeign = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AboutTheProject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnvironmentalImpact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocalManpower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ForeignManpower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TrainingPrograms = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExperienceOfInvestor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationData = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    SecondPhoneNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreationData = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NationalityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Nationality_NationalityId",
+                        column: x => x.NationalityId,
+                        principalTable: "Nationality",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    SecondPhoneNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IDNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreationData = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NationalityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Investor_Nationality_NationalityId",
+                        column: x => x.NationalityId,
+                        principalTable: "Nationality",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "License",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpireDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LicenseNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_License", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_License_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advertisement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advertisement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Advertisement_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectCoordinators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectCoordinators", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectCoordinators_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProjectCoordinators_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectInvestors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    InvestorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectInvestors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectInvestors_Investor_InvestorId",
+                        column: x => x.InvestorId,
+                        principalTable: "Investor",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProjectInvestors_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CoordinatorReport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreationData = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsUpdated = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateData = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProjectCoordinatorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoordinatorReport", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CoordinatorReport_ProjectCoordinators_ProjectCoordinatorId",
+                        column: x => x.ProjectCoordinatorId,
+                        principalTable: "ProjectCoordinators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisement_EmployeeId",
+                table: "Advertisement",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CoordinatorReport_ProjectCoordinatorId",
+                table: "CoordinatorReport",
+                column: "ProjectCoordinatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_NationalityId",
+                table: "Employee",
+                column: "NationalityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investor_NationalityId",
+                table: "Investor",
+                column: "NationalityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_License_ProjectId",
+                table: "License",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectCoordinators_EmployeeId_ProjectId",
+                table: "ProjectCoordinators",
+                columns: new[] { "EmployeeId", "ProjectId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectCoordinators_ProjectId",
+                table: "ProjectCoordinators",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectInvestors_InvestorId_ProjectId",
+                table: "ProjectInvestors",
+                columns: new[] { "InvestorId", "ProjectId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectInvestors_ProjectId",
+                table: "ProjectInvestors",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Advertisement");
+
+            migrationBuilder.DropTable(
+                name: "CoordinatorReport");
+
+            migrationBuilder.DropTable(
+                name: "License");
+
+            migrationBuilder.DropTable(
+                name: "ProjectInvestors");
+
+            migrationBuilder.DropTable(
+                name: "ProjectCoordinators");
+
+            migrationBuilder.DropTable(
+                name: "Investor");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Nationality");
+        }
+    }
+}
