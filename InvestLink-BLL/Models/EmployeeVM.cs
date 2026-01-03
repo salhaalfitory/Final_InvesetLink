2683c1ec82470a54dde07c167397df69f0e9bffc
salhaalfitory <salhaalfitory791@gmail.com>
2026-01-03 03:28:46 +0200

    .

--- InvestLink-BLL/Models/EmployeeVM.cs
+++ InvestLink-BLL/Models/EmployeeVM.cs
@@ -10,6 +10,13 @@ namespace InvestLink_BLL.Models
 {
     public class EmployeeVM
     {
+        public EmployeeVM()
+        {
+            this.IsActive = true;
+            this.IsDeleted = false;
+            this.CreationData = DateTime.Now;
+        }
+
         [Key]
         public int Id { get; set; }
         [Required, StringLength(100)]
@@ -24,7 +31,7 @@ namespace InvestLink_BLL.Models
         [MinLength(10)]
         [MaxLength(10)]
         public string? SecondPhoneNumber { get; set; }//رقم تلفون2
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } 
         public bool IsDeleted { get; set; }
         public DateTime CreationData { get; set; } = DateTime.Now;//تاريخ تسجيل
         //------------------------------
