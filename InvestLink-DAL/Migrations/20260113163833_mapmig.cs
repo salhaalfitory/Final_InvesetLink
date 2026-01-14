9aff10473597a8dbc1a142f7582e91710a0c6ba9
رهام الوزري <rehamalwezri@gmail.com>
2026-01-14 02:27:33 +0200

    ;

---
+++ InvestLink-DAL/Migrations/20260113163833_mapmig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestLink_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class mapmig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
